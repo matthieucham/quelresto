5c377c49945d15e18595a72da6802e4bb2a2b063
Matthieu Grandrie <matthieu.grandrie@lyra-network.com>
2015-10-13 12:47:49 +0200

    SecurityMiddleware

--- quelresto_backend/quelresto/settings.py
+++ quelresto_backend/quelresto/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    #'django.middleware.security.SecurityMiddleware',
 
     'quelresto_app.middleware.RecognizeParticipantMiddleware',
 )
