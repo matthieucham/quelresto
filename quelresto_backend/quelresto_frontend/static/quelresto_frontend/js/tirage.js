85dcc3fbd0d0c05c223d14783ef9b967a2467f4d
matthieucham <matthieu.cham@gmail.com>
2016-01-20 23:05:32 +0100

    Début 2e page : liste des restos

--- quelresto_backend/quelresto_frontend/static/quelresto_frontend/js/tirage.js
+++ quelresto_backend/quelresto_frontend/static/quelresto_frontend/js/tirage.js
@@ -4,12 +4,23 @@ $(function() {
     // Récup infos tirage
     var tirage;
 
+    $.get('../../rest/restos/', function(resp) {
+        makeRestosList(resp);
+    },'json')
+
     $.get('../../rest/tirages/'+tirageId+'/', function(resp) {
         updateTirage(resp);
     }, 'json')
 
     function updateTirage(resp) {
         tirage = resp;
-        $('#h-title').text("Tirage initié par "+tirage.master);
+        $('#p-name').text(tirage.uuid+"   Tirage initié par "+tirage.master);
+    }
+
+    function makeRestosList(data) {
+        data.forEach( function(resto) {
+            $('#group-restos').append('<button type="button" class="btn btn-info btn-lg" data-toggle="button" autocomplete="off">'+resto.nom+'</button>')
+        });
+        $('#group-restos').append('<input class="form-control input-lg" id="inputlg" type="text" placeholder="Autre proposition"/>');
     }
 })
