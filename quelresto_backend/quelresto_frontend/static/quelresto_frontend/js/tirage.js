cd14fd1e97645e777e54d180a04989d21f910537
matthieucham <matthieu.cham@gmail.com>
2016-01-25 23:44:30 +0100

    Bordel de merde

--- quelresto_backend/quelresto_frontend/static/quelresto_frontend/js/tirage.js
+++ quelresto_backend/quelresto_frontend/static/quelresto_frontend/js/tirage.js
@@ -29,4 +29,23 @@ $(function() {
         });
         $('#group-restos').append('<input class="form-control input-lg" id="inputlg" type="text" placeholder="Autre proposition"/>');
     }
+
+    $('#btn-selection').click(function() {validateChoices()});
+
+    function validateChoices() {
+        // Tous les boutons activés de group-restos + l'input supplémentaire eventuellement
+        var sel = [];
+        $('#group-restos button.active').each(function(){
+            var choice = {"nom": $( this ).text() };
+            //choice['nom'] = $( this ).text();
+            sel.push(choice);
+        })
+        if ($('#inputlg').val().length > 0) {
+            var choice = {"nom": $('#inputlg').val()};
+            //choice['nom'] = $('#inputlg').val();
+            sel.push(choice);
+        }
+        // Envoi au serveur
+        $.post('../../rest/tirages/'+tirage.uuid+'/', JSON.stringify({"selections" : sel}), updateTirage, 'json');
+    }
 })
